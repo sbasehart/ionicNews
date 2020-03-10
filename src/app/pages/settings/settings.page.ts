6d31b6c8501267c9fe38dc615bf28b0ffbbd129c
Summer Basehart <summer@Judes-Mac-mini.local>
2020-03-10 15:42:41 -0700

    button animations: fadeIn

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private storage: Storage, private alertController: AlertController, public toastController: ToastController) { }
 
   ngOnInit() {
+  }
+
+  async onClearFavorites() {
+    const alert = await this.alertController.create({
+      header: 'Please confirm',
+      message: 'Delete All Favorites?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Unfavorite cancelled');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.storage.remove('favorite')
+            this.favoritesRemoved()
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async favoritesRemoved() {
+    const toast = await this.toastController.create({
+      message: `All Favorites Removed`,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
